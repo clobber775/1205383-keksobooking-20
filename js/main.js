49ffbb59e8c37659c461b9eaab0729a822b5ed44
clobber775 <warez3001@gmail.com>
2020-06-05 19:31:06 +0300

    m3t2

--- js/main.js
+++ js/main.js
@@ -2,14 +2,15 @@
 
 var FIRST_OBJECT = 0;
 var NUMBER_OF_OBJECTS = 8;
-var TYPE = ['place', 'flat', 'house', 'bungalo'];
-var CHECKIN = ['12:00', '13:00', '14:00'];
+var TYPES = ['place', 'flat', 'house', 'bungalo'];
+var CHECKINS = ['12:00', '13:00', '14:00'];
 var FEAUTERS = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
 var PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
 var FIRST_AVATAR = 1;
 var LAST_AVATAR = 8;
 var PIN_POS_Y_START = 130;
 var PIN_POS_Y_END = 630;
+var PIN_POS_X_START = 0;
 
 var getRandomNumberInRange = function (min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -24,7 +25,7 @@ function shuffleArray(array) {
   }
 }
 
-var getRandomMassiveLength = function (array) {
+var getRandomArrayLength = function (array) {
   shuffleArray(array);
   return array.slice([getRandomNumberInRange(FIRST_OBJECT, array.length + 1)]);
 };
@@ -39,20 +40,20 @@ var createObject = function (array) {
           avatar: 'img/avatars/user0' + getRandomNumberInRange(FIRST_AVATAR, LAST_AVATAR) + '.png'
         },
         location: {
-          x: getRandomNumberInRange(FIRST_OBJECT, width),
+          x: getRandomNumberInRange(PIN_POS_X_START, width),
           y: getRandomNumberInRange(PIN_POS_Y_START, PIN_POS_Y_END),
         },
         offer: {
           title: 'Объявление',
           address: location.x + ',' + location.y,
           price: 100,
-          type: TYPE[getRandomNumberInRange(FIRST_OBJECT, TYPE.length - 1)],
+          type: TYPES[getRandomNumberInRange(FIRST_OBJECT, TYPES.length - 1)],
           rooms: 3,
           guests: 456,
-          checkin: CHECKIN[[getRandomNumberInRange(FIRST_OBJECT, CHECKIN.length - 1)]],
-          checkout: CHECKIN[[getRandomNumberInRange(FIRST_OBJECT, CHECKIN.length - 1)]],
-          features: getRandomMassiveLength(FEAUTERS),
-          photos: getRandomMassiveLength(PHOTOS),
+          checkin: CHECKINS[[getRandomNumberInRange(FIRST_OBJECT, CHECKINS.length - 1)]],
+          checkout: CHECKINS[[getRandomNumberInRange(FIRST_OBJECT, CHECKINS.length - 1)]],
+          features: getRandomArrayLength(FEAUTERS),
+          photos: getRandomArrayLength(PHOTOS),
         }
       };
   }
@@ -68,8 +69,6 @@ var pinTemplate = document.querySelector('#pin')
   .content
   .querySelector('.map__pin');
 
-var pins = createObject([]);
-
 var renderPins = function (obj) {
   var newpinsElement = pinTemplate.cloneNode(true);
   newpinsElement.querySelector('img').src = obj[i].author.avatar;
@@ -80,6 +79,6 @@ var renderPins = function (obj) {
 
 var fragment = document.createDocumentFragment();
 for (var i = FIRST_OBJECT; i < NUMBER_OF_OBJECTS; i++) {
-  fragment.appendChild(renderPins(pins));
+  fragment.appendChild(renderPins(createObject([])));
 }
 pinElement.appendChild(fragment);
